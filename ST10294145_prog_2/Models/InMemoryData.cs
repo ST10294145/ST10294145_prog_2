a3776d262f5a9aaa1e2b87676066e96965a3fddd
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-10-16 21:44:05 +0200

    InMemoryData model made. To simulate a database, create a static class for storing claims and users in memory.

---
+++ ST10294145_prog_2/Models/InMemoryData.cs
@@ -0,0 +1,16 @@
+﻿namespace ST10294145_prog_2.Models
+{
+    namespace ST10294145_prog_2.Models
+    {
+        public static class InMemoryData
+        {
+            public static List<Claim> Claims { get; set; } = new List<Claim>();
+            public static List<User> Users { get; set; } = new List<User>
+        {
+            new User { Username = "lecturer", Password = "password", Role = "Lecturer" },
+            new User { Username = "coordinator", Password = "password", Role = "Coordinator" },
+            new User { Username = "manager", Password = "password", Role = "Manager" }
+        };
+        }
+    }
+}
