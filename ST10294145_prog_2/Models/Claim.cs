a8ab4b3547e3b848f887a3c4f2dcaa0b8c03623f
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-10-16 21:21:49 +0200

    Created a claim model class to store LecturerName, HouseWorked,HourlyRate,Status,SupportingDocument.

---
+++ ST10294145_prog_2/Models/Claim.cs
@@ -0,0 +1,12 @@
+﻿namespace ST10294145_prog_2.Models
+{
+    public class Claim
+    {
+        public int Id { get; set; }
+        public string LecturerName { get; set; }
+        public double HoursWorked { get; set; }
+        public double HourlyRate { get; set; }
+        public string Status { get; set; } = "Pending";
+        public string SupportingDocument { get; set; }
+    }
+}
