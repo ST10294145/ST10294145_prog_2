53941f3c4605886452649420a955219f79258e42
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-10-16 21:32:20 +0200

    Created a User Model to seperate the different users like "Lecturer", "Coordinator", "Manager". These will also be turned into roles.

---
+++ ST10294145_prog_2/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ST10294145_prog_2.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } // "Lecturer", "Coordinator", "Manager"
+    }
+}
